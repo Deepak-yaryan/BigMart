fc79e05332c663923ee62d8ba0581f228e457e34
Deepak <deepakyaryan898@gmail.com>
2023-06-12 19:54:51 +0530

    Initial Commit

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,14 +4,15 @@ let CryptoJS = require("crypto-js")
 let jwt = require("jsonwebtoken")
 
 const handler = async (req, res) => {
+    const AES = process.env.AES_SECRET;
+    const JWT = process.env.JWT_SECRET;
     if (req.method == "POST") {
-        console.log(req.body);
         let user = await User.findOne({ "email": req.body.email })
         if (user) {
-        const bytes = CryptoJS.AES.decrypt(user.password, process.env.AES_SECRET);
+        const bytes = CryptoJS.AES.decrypt(user.password, AES);
         let decryptedPass = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
             if (req.body.email == user.email && req.body.password == decryptedPass) {
-                let token = jwt.sign({ email: user.email, name: user.name }, process.env.JWT_SECRET, { expiresIn: "2d" });
+                let token = jwt.sign({ email: user.email, name: user.name }, JWT, { expiresIn: "2d" });
                 res.status(200).json({ Success: true, token: JSON.stringify(token) });
                 console.log(token);
             }
