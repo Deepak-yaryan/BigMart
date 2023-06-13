9274ca6981b687710e64c848c8f2ede59acbc16e
Deepak <deepakyaryan898@gmail.com>
2023-06-13 23:59:23 +0530

    Initial Commit

--- pages/orders.js
+++ pages/orders.js
@@ -39,7 +39,7 @@ const Orders = () => {
             return <div key = {item._id} className="order xl:w-full py-4 px-0.5">
                 <Link href={'/order?id=' + item._id} legacyBehavior>
                 <a>
-                <div className="flex justify-center items-center shadow-sm shadow-pink-500 py-6 rounded-lg space-x-1">
+                <div className="flex justify-center items-center shadow-sm shadow-pink-500 py-2 rounded-lg space-x-1">
                     <div className="w-1/5 inline-flex items-center justify-center">
                         <img className='md:h-24 h-11 hover:scale-110 duration-200 rounded' src={item.products[`${Object.keys(item.products)}`]['img']} alt="Order Image" />
                     </div>
